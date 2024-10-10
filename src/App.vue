d697e87371a9d6dbab327affa7b65640c5244e20
Kabelo Peele <kabelom.p@live.com>
2024-10-10 14:01:33 +0200

    Created 'SearchField' component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup>
+import SearchField from '@/components/SearchField.vue';
+</script>
+
 <template>
-  <main>Start</main>
+  <main>
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{
+        new Date().toLocaleDateString('en-za', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })
+      }}
+    </div>
+    <!-- Search Field -->
+    <SearchField />
+  </main>
 </template>
